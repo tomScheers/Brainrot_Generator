d3ba0f4897a479c414e75143a929a44c280ed304
Tom Scheers <tomscheers@outlook.com>
2024-08-03 20:59:51 +0200

    Added it so that the video and the audio get merged

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tts
 import reddit
-from moviepy.editor import VideoFileClip, AudioFileClip
+import subprocess
+from mutagen.mp3 import MP3
+import time
 
 
 # main mod
@@ -9,10 +11,43 @@ def main():
     story_txt = story["data"]["children"][1]["data"]["selftext"]
     audio_path = "./Vid_Com/story.mp3"
     tts.text_to_speech(story_txt, path=audio_path)
-    video = VideoFileClip("./Vid_Com/vid.mp4")
-    audio = AudioFileClip(audio_path)
-    video = video.set_audio(audio)
-    video.write_videofile("result.mp4", codec='mpeg4', audio_codec='libvorbis')
+
+    audio = MP3(audio_path)
+    audio_length = audio.info.length
+
+    video_path = "./Vid_Com/vid.mp4"
+    output_path = "result.mp4"
+    trimmed_video_path = "./Vid_Com/trimmed_vid.mp4"
+
+    start_time_seconds = 20
+    end_time_seconds = start_time_seconds + audio_length + 3
+
+    time_format = "%H:%M:%S"
+    formatted_start_time = time.strftime(time_format, time.gmtime(start_time_seconds))
+    formatted_end_time = time.strftime(time_format, time.gmtime(end_time_seconds))
+
+
+    # Trim video and merge with audio in one go
+    subprocess.run([
+        "ffmpeg",
+        "-i", video_path,
+        "-ss", formatted_start_time,
+        "-to", formatted_end_time,
+        "-c:v", "copy",
+        "-c:a", "aac",
+        "-strict", "experimental",
+        trimmed_video_path
+    ])
+
+    subprocess.run([
+        "ffmpeg",
+        "-i", trimmed_video_path,
+        "-i", audio_path,
+        "-c:v", "copy",
+        "-c:a", "aac",
+        "-strict", "experimental",
+        output_path
+    ])
 
 
 
--- tts.py
+++ tts.py
@@ -1,10 +1,23 @@
 from gtts import gTTS
 import os
+import subprocess
 
 
 def text_to_speech(text, path="story.mp3", lang='en', tld="us"):
     obj = gTTS(text=text, lang=lang, slow=False, tld=tld)
-    obj.save(path)
+    temp_path = "temp.mp3"
+    obj.save(temp_path)
+    speed_up_audio(temp_path, path)
+
+
+def speed_up_audio(input_path, output_path, speed=1.3):
+    subprocess.run([
+        "ffmpeg",
+        "-i", input_path,
+        "-filter:a", f"atempo={speed}",
+        "-vn",
+        output_path
+    ])
 
 
 if __name__ == '__main__':
