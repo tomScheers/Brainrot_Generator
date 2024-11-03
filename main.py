6b20650f4d1103958d38ea459c83d9994dc551e4
Tom Scheers <tomscheers@outlook.com>
2024-11-03 17:54:36 +0100

    Made some changes and added them to github

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import tts
-import reddit
 import subprocess
-from mutagen.mp3 import MP3
 import time
 
+from mutagen.mp3 import MP3
 
-# main mod
+import reddit
+# import transcript
+import tts
+
+
+def format_time(seconds, time_format):
+    return time.strftime(time_format, time.gmtime(seconds))
+
+
 def main():
     story = reddit.get_post("AmItheAsshole")
     story_txt = story["data"]["children"][1]["data"]["selftext"]
@@ -16,20 +22,23 @@ def main():
     audio_length = audio.info.length
 
     video_path = "./Vid_Com/vid.mp4"
-    output_path = "result.mp4"
+    output_path = "Output/result.mp4"
     trimmed_video_path = "./Vid_Com/trimmed_vid.mp4"
 
     start_time_seconds = 20
-    end_time_seconds = start_time_seconds + audio_length + 3
+    end_time_padding = 3
+    end_time_seconds = start_time_seconds + audio_length + end_time_padding
 
     time_format = "%H:%M:%S"
-    formatted_start_time = time.strftime(time_format, time.gmtime(start_time_seconds))
-    formatted_end_time = time.strftime(time_format, time.gmtime(end_time_seconds))
+    formatted_start_time = format_time(start_time_seconds, time_format)
+    formatted_end_time = format_time(end_time_seconds, time_format)
 
 
-    # Trim video and merge with audio in one go
+    # Trim video to be the appropiate length based on the mp3 length
     subprocess.run([
-        "ffmpeg",
+        "ffmpeg", "-y",
+        "-hide_banner",
+        "-loglevel", "error",
         "-i", video_path,
         "-ss", formatted_start_time,
         "-to", formatted_end_time,
@@ -39,16 +48,24 @@ def main():
         trimmed_video_path
     ])
 
+    # Merge audio and video file
     subprocess.run([
-        "ffmpeg",
+        "ffmpeg", "-y",
+        "-hide_banner",
+        "-loglevel", "error",
         "-i", trimmed_video_path,
         "-i", audio_path,
         "-c:v", "copy",
-        "-c:a", "aac",
-        "-strict", "experimental",
-        output_path
+        "-map", "0:v:0",
+        "-map", "1:a:0",
+        "-shortest", output_path
     ])
 
+    subprocess.run([
+        "rm", trimmed_video_path, audio_path
+    ])
+
+    # transcript.add_subtitles_to_video(output_path, story_txt, "./output/subtitles.mp4")
 
 
 if __name__ == "__main__":
--- tts.py
+++ tts.py
@@ -1,26 +1,41 @@
 from gtts import gTTS
 import os
 import subprocess
+import re
 
 
-def text_to_speech(text, path="story.mp3", lang='en', tld="us"):
+def sanitize_text(text):
+    translation_dict = {r"aita": "am I the asshole", r"(\d+)m": r"\1em"}
+
+    new_text = text
+    for translation_key, translation_val in translation_dict.items():
+        new_text = re.sub(translation_key,
+                          translation_val,
+                          new_text,
+                          flags=re.IGNORECASE)
+    print(new_text)
+    return new_text
+
+
+def text_to_speech(text,
+                   path="Vid_Com/story.mp3",
+                   lang='en',
+                   tld="us",
+                   speed=1.3):
+    text = sanitize_text(text)
     obj = gTTS(text=text, lang=lang, slow=False, tld=tld)
-    temp_path = "temp.mp3"
+    temp_path = "Vid_Com/temp.mp3"
     obj.save(temp_path)
-    speed_up_audio(temp_path, path)
+    speed_up_audio(temp_path, path, speed)
+    os.remove(temp_path)
 
 
 def speed_up_audio(input_path, output_path, speed=1.3):
     subprocess.run([
-        "ffmpeg",
-        "-i", input_path,
-        "-filter:a", f"atempo={speed}",
-        "-vn",
-        output_path
+        "ffmpeg", "-y", "-hide_banner", "-loglevel", "error", "-i", input_path,
+        "-filter:a", f"atempo={speed}", "-vn", output_path
     ])
 
 
 if __name__ == '__main__':
-    print("Hello World")
-    text_to_speech("Hello beautiful world!")
-    os.system("play story.mp3")
+    print(sanitize_text("aita? I 39m"))
